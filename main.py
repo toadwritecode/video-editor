05451e1e8d92e3711860a20ec877445ce82d1053
Maks <mlebedko@sdvor.com>
2024-04-28 18:52:43 +0500

    имитация аутентификации через basic auth

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI, UploadFile, File, Depends, APIRouter, Query
 from starlette.responses import FileResponse, JSONResponse
 
-from async_tasks import create_task, test, get_result_task
+from async_tasks import create_task, get_result_task
 from conf.config import BASE_DIR
 from schemas.actions_schema import CutSchema
-from security import get_basic_auth
-from services.video_handler import cut_video_file, extract_audio_from_video_file, transcribe_audio
+from security import get_basic_auth, authenticate_user
+from services.video_handlers import cut_video_file, extract_audio_from_video_file, transcribe_audio
 
 app = FastAPI()
 
 video_router = APIRouter(dependencies=[Depends(get_basic_auth)], prefix='/video')
-
+auth_router = APIRouter()
 VIDEO_STORE_DIR = BASE_DIR / "video"
 
 
@@ -50,4 +50,11 @@ async def exact_audio_from_video_file(task_id: str = Query(alias="taskId")):
     return JSONResponse(status_code=201, content={'status': 'ok' if result else 'processing', 'result': result})
 
 
+@auth_router.get("/token")
+async def get_token(username: str, password: str):
+    token = authenticate_user(username, password)
+    return JSONResponse(status_code=200, content={"token": token})
+
+
 app.include_router(video_router)
+app.include_router(auth_router)
--- security.py
+++ security.py
@@ -1,4 +1,5 @@
 import secrets
+from base64 import b64encode
 
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
@@ -9,11 +10,10 @@ from conf.config import settings
 security = HTTPBasic()
 
 
-def get_basic_auth(credentials: HTTPBasicCredentials = Depends(security)):
-
+def check_auth(username: str, password: str):
     # сверка по хешу знач
-    is_user_ok = secrets.compare_digest(credentials.username, settings.AUTH_API_LOGIN)
-    is_pass_ok = secrets.compare_digest(credentials.password, settings.AUTH_API_PASSWORD)
+    is_user_ok = secrets.compare_digest(username, settings.AUTH_API_LOGIN)
+    is_pass_ok = secrets.compare_digest(password, settings.AUTH_API_PASSWORD)
 
     if not is_user_ok or not is_pass_ok:
         raise HTTPException(
@@ -21,3 +21,12 @@ def get_basic_auth(credentials: HTTPBasicCredentials = Depends(security)):
             detail="Incorrect login or password",
             headers={"WWW-Authenticate": "Basic"},
         )
+
+
+def get_basic_auth(credentials: HTTPBasicCredentials = Depends(security)):
+    check_auth(credentials.username, credentials.password)
+
+
+def authenticate_user(username: str, password: str):
+    check_auth(username, password)
+    return f'Basic {b64encode(f"{username}:{password}".encode("utf-8")).decode("ascii")}'
