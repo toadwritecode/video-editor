5b2b514271aa9298c1620483ea34a91a75e58757
Maks <mlebedko@sdvor.com>
2024-05-02 22:48:11 +0500

    обернул в докер

---
+++ src/conf/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class Settings(BaseSettings):
+    AUTH_API_LOGIN: str = ''
+    AUTH_API_PASSWORD: str = ''
+    SECURITY_ENABLED: bool = False
+    STORAGE_NAME: str = "storage"
+
+    class Config:
+        case_sensitive = True
+        env_file = BASE_DIR / ".env"
+
+
+settings = Settings()
